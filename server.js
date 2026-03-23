318698fd2eb0d0a57a247613f9dd7a0b14653b66
Michael Otieno <emotyworks@yahoo.com>
2026-03-23 14:10:14 +0300

    Add ultra-modern luxury frontend design (Nicholas Experience)

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-// Basic route
+// Serve static files (VERY IMPORTANT)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("Luxury Real Estate Platform is live 🚀");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/properties", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "properties.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "contact.html"));
 });
 
 app.listen(PORT, () => {
